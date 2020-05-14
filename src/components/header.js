4065973b43ea8e6efea6110c2165f71e3611866d
Muhammad Tifak <moht20009@gmail.com>
2020-05-14 08:47:57 -0700

    Added header

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './header.css';
+
+
+class Header extends Component {
+  render () {
+    return (
+      <header className='header'>
+        <h1>
+          Picture Perfect Playground Alliteration
+        </h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
