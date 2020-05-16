3c32a11bf85d3d0fd19f2eddabab9d2c393033c4
Muhammad Tifak <moht20009@gmail.com>
2020-05-15 17:13:12 -0700

    Updates

---
+++ precache-manifest.c64fb28c47f0e0d14c44c81178871613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e15bff1655a1b0d4f9fc2e3152c5848",
+    "url": "/pic-perfect/index.html"
+  },
+  {
+    "revision": "71df02da6d2bd4562bfa",
+    "url": "/pic-perfect/static/css/main.1e673f87.chunk.css"
+  },
+  {
+    "revision": "c63aa08337de58952ba0",
+    "url": "/pic-perfect/static/js/2.62260c4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pic-perfect/static/js/2.62260c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71df02da6d2bd4562bfa",
+    "url": "/pic-perfect/static/js/main.ed71c92e.chunk.js"
+  },
+  {
+    "revision": "934ce80b910e2c9b937f",
+    "url": "/pic-perfect/static/js/runtime-main.d057bcb3.js"
+  }
+]);
